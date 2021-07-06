f864af8dabe2a72050eed75266884359c82c22a4
JohnCoene <jcoenep@gmail.com>
2021-07-06 19:38:34 +0200

    add theming

---
+++ R/theme.R
@@ -0,0 +1,80 @@
+#' Theme
+#' 
+#' Convenience function to create a theme.
+#' 
+#' @param name Name of theme.
+#' @param box_bg,box_color,content_bg,content_color Background and text color respectively.
+#' @param box_font_size,content_font_size Size of the font.
+#' @param arrow_color Color of the arrow (if enabled).
+#' 
+#' @examples 
+#' library(shiny)
+#' 
+#' ui <- fluidPage(
+#'   useTippy(),
+#'   tippyTheme("myTheme", box_bg = "darkred"),
+#'   h5("plot tooltip", id = "plotTip"),
+#'   tippyThis(
+#'     "plotTip",
+#'     "Hello world",
+#'     theme = "myTheme"
+#'   )
+#' )
+#' 
+#' server <- function(input, output) {}
+#' 
+#' if(interactive())
+#'   shinyApp(ui, server)
+#' 
+#' @importFrom shiny tags
+#' 
+#' @export 
+tippyTheme <- function(
+	name, 
+	box_bg = "black", 
+	box_color = "white",
+	box_font_size = "12px",
+	arrow_color = box_bg,
+	content_bg = box_bg,
+	content_color = box_color,
+	content_font_size = box_font_size
+){
+
+	if(missing(name))
+		stop("Missing `name`")
+
+	theme <- sprintf(
+		".tippy-box[data-theme~='%s'] {
+			background-color: %s;
+			color: %s;
+			font-size: %s;
+		}
+		.tippy-box[data-theme~='%s'] > .tippy-arrow {
+			color: %s;
+		} 
+		.tippy-box[data-theme~='%s'] > .tippy-content {
+			background-color: %s;
+			color: %s;
+			font-size: %s;
+		}",
+		name,
+		box_bg,
+		box_color,
+		box_font_size,
+		name,
+		arrow_color,
+		name,
+		content_bg,
+		content_color,
+		content_font_size
+	)
+
+	tags$head(
+		HTML(
+			sprintf(
+				"<style>%s</style>",
+				theme
+			)
+		)
+	)
+}
